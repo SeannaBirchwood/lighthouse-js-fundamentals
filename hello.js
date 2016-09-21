5bc0eac964e1356c43c85d90a312fdab5773950e
SeannaBirchwood <chelsea.craig1223@gmail.com>
2016-09-21 00:07:24 +0000

    Adds functions exercises

---
+++ hello.js
@@ -0,0 +1,14 @@
+function sayHello() {
+	console.log("Hello, world");
+}
+
+sayHello();
+
+Function sayHello(name) {
+	console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
